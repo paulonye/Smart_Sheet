d2cde877717c3525387cb968ac1517a73711245d
paulonye <nwosupaul141@gmail.com>
2023-01-07 07:20:47 +0000

    Added error catching functionality

---
+++ send_mail.py
@@ -0,0 +1,30 @@
+"""This script is used to send mails"""
+
+import smtplib
+from email.message import EmailMessage
+import os
+
+
+def send_email(error_msg):
+    """This function sends a mail to key
+    stakeholdrs if the pipeline breaks at any point.
+
+    Allows personnel to catch the error and fix"""
+
+    EMAIL_ADDRESS = 'use your email address'
+    EMAIL_PASSWORD = 'use the app password gmail generated for you'
+
+    # New Email Message
+    msg = EmailMessage()
+    msg['Subject'] = 'BROKEN PIPELINE'
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = ['the email addres(es) you need to send it to']
+    msg.set_content(f'Logged Error:/n {error_msg}')
+
+    # Send the message
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.send_message(msg)
+
+if __name__== '__main__':
+    send_email('Just_testing')
