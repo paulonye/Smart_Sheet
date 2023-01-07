705388eed5cd97e1eab805a27384aea511070e9e
paulonye <nwosupaul141@gmail.com>
2023-01-07 07:19:41 +0000

    Made minor changes

--- append.py
+++ append.py
@@ -12,15 +12,15 @@ except Exception as e:
     send_mail.send_email(e)
     os._exit(0)
 
-#This imports the service account credentials that will allow us access the sheet
+
 try:
+    #This imports the service account credentials that will allow us access the sheet
     client = get_connect_sheet()
-#Once the connection has been made, the Google Sheet can now be accessed
+    #Once the connection has been made, the Google Sheet can now be accessed
     push_sheet = client.open('test_sheet')
 except Exception as e:
     send_mail.send_email(e)
     os._exit(0)
-
 
 def append_new_data(df, sheet_name):
     """This function takes in the dataframe and the name of the sheet you wish
